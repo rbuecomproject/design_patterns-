d9fd45f18fedec0bd0ff42419013efedf71b1150
rbuecomproject <144703951+rbuecomproject@users.noreply.github.com>
2024-04-01 00:16:57 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,38 @@
+// Employee class
+class Employee {
+    // Member variables
+    private String name;
+    private int employeeId;
+    private double hourlyRate;
+    private double hoursWorked;
+
+    // Constructor
+    public Employee(String name, int employeeId, double hourlyRate) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = 0; // Initially, no hours worked
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    // Method to set hours worked
+    public void setHoursWorked(double hours) {
+        this.hoursWorked = hours;
+    }
+}
